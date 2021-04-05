dc50146ac23270b894feebba87b4f0ca6e9c7972
Alexey Kabikov <Alexey.Kabikov@gmail.com>
2021-04-05 14:47:06 +0300

    feat: add previews to bookmarks

--- components/Bookmarks/index.jsx
+++ components/Bookmarks/index.jsx
@@ -1,17 +1,30 @@
-function Bookmarks({ list, handleRemoveBookmark }) {
+import { useState, useEffect } from "react";
+import PreviewList from "../PreviewList";
+import { getVideoById } from "../../helpers/loading";
+
+function Bookmarks({ list, removeBookmark }) {
+  const [state, setState] = useState(null);
+
+  useEffect(() => {
+    async function getVideos() {
+      const videos = await Promise.all(list.map((id) => getVideoById(id)));
+      setState(
+        videos.map(({ title, url, preview }, i) => ({
+          id: list[i],
+          title,
+          url,
+          preview: preview.small,
+        }))
+      );
+    }
+
+    getVideos();
+  }, [list]);
+
   return (
     <>
       <h2>Bookmarks</h2>
-      <ul>
-        {list.map((id) => (
-          <li key={id}>
-            {id}{" "}
-            <button type='button' onClick={() => handleRemoveBookmark(id)}>
-              Remove bookmark
-            </button>
-          </li>
-        ))}
-      </ul>
+      {state && <PreviewList list={state} removeBookmark={removeBookmark} />}
     </>
   );
 }
