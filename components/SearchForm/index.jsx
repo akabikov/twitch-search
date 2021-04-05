3a0cf5daf7ebe870b8d6285a902484a8dcbce5ef
Alexey Kabikov <Alexey.Kabikov@gmail.com>
2021-04-05 20:10:31 +0300

    style: styling the search form

--- components/SearchForm/index.jsx
+++ components/SearchForm/index.jsx
@@ -1,19 +1,23 @@
 import { useState } from "react";
+import styles from "./index.module.scss";
 
 function SearchForm({ value }) {
   const [val, setVal] = useState(value);
 
   return (
-    <form>
+    <form className={styles.SearchForm}>
+      <label htmlFor='search'>Канал Twitch</label>
       <input
         type='search'
         name='q'
-        placeholder='Search...'
+        id='search'
+        placeholder='etoleto'
         autoComplete='off'
+        className={styles.input}
         value={val}
         onChange={({ target }) => setVal(target.value)}
       />
-      <input type='submit' value='Search' />
+      <input type='submit' value='Найти' />
     </form>
   );
 }
